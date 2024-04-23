60a56c1f67d0c74dc1e0a343ba26e9f952b62af6
Kelsey Mcnutt <kelseymcnutt@Kelseys-Air.home>
2024-04-23 11:00:30 -0500

    endpoint for stories

--- BuccaneerBackend/Program.cs
+++ BuccaneerBackend/Program.cs
@@ -1,3 +1,5 @@
+using BuccaneerBanter.Models.DTOs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,29 +18,20 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapGet("/stories" , () => {
+    return stories.Select(s => new StoryDTO
+    {
+        Id = s.Id,
+        Title = s.Title,
+        Pirate = new PirateDTO 
+        {
+            Id = s.Pirate.Id,
+            Name = s.Pirate.Name
+        },
+        Content = s.Content,
+        Date = s.Date
+    });
+});
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
